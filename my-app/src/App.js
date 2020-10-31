d7a674b4531bf6b24238825c453f5dc4fbb265c4
Arnavjain1703 <arnavjainkht@gmail.com>
2020-10-31 11:03:52 +0530

    Bootstrap Setup

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import {BrowserRouter} from 'react-router-dom'
+import {Route,Switch} from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-success">hello</button>
+    </div>
+  );
+}
+
+export default App;
